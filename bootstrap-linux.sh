56cb458ee5276e1a9f8259855104497d86dbef13
Sam Balana <sbalana@google.com>
2024-08-27 11:07:30 -0700

    Fix bootstrap apt packages

--- bootstrap-linux.sh
+++ bootstrap-linux.sh
@@ -6,10 +6,8 @@ apt full-upgrade --yes
 
 # Install Neovim
 if ! hash nvim 2>/dev/null; then
-  apt install software-properties-common
-  add-apt-repository ppa:neovim-ppa/stable
   apt update
-  apt install neovim python-dev python-pip python3-dev python3-pip xsel
+  apt install neovim python3-dev python3-pip python3-neovim xsel
 
   # Set Neovim to default editor
   update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
@@ -25,6 +23,7 @@ apt install git
 apt install stow
 apt install tmux
 apt install wget
+apt install fd-find
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh /dev/stdin -y --no-modify-path
